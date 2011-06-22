a2a4ba84d8154aaafc07019657b375a81ca20dd4
Daniel Davies <daniel@danux.co.uk>
2011-06-22 16:13:24 +0100

    Tidied up settings

--- soj2/settings.py
+++ soj2/settings.py
@@ -2,51 +2,28 @@ import os
 
 from mcnulty.settings import *
 
+
 PROJECT_ROOT = os.path.dirname(__file__)
-
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
-
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),) + TEMPLATE_DIRS
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-# Set this to a unique, random phrase and keep it secret.
 
+# Set this to a unique, random phrase and keep it secret.
 SECRET_KEY = "^brmcy8*+lt!i8fwjceco67&1+d3tn2*@xv4_b1%)9!u+9pj6("
 
 # If you are running multiple Vertex sites on this server, set this to a unique
 # phrase.
-
 CACHE_MIDDLEWARE_KEY_PREFIX = "soj2"
 
 # Vertex will send you notifications of broken links found on your website.
 # Change this to provide a helpful prefix for all of these emails to help keep
 # your inbox well-sorted.
-
 EMAIL_SUBJECT_PREFIX = "[SOJ2]"
-
-# Database settings.  Leave these as they are to use the default built-in
-# SQLite database.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'soj2.sqlite',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-# Email settings.
-SERVER_EMAIL = "daniel@amarus.co.uk"
-DEFAULT_FROM_EMAIL = "daniel@amarus.co.uk"
 
 # This setting installs the base set of Vertex applications.  Add your own
 # custom applications to enable additional functionality for your site.
-
-INSTALLED_APPS += ("django.contrib.staticfiles",
-                   "sorl.thumbnail",
+INSTALLED_APPS += ("sorl.thumbnail",
                    "registration",
                    "django_coverage",
                    "mcnulty.contentpages",
@@ -58,26 +35,23 @@ INSTALLED_APPS += ("django.contrib.staticfiles",
                    "soj2.characters",
                    "soj2.game")
 
+# Accounts Settings
 ACCOUNT_ACTIVATION_DAYS = 7
-
 ROOT_URLCONF = "soj2.urls"
+LOGIN_REDIRECT_URL = '/accounts/login/'
 
-ADMIN_MEDIA_PREFIX = "/media/lib/admin/"
-
-STATIC_DOC_ROOT = '/srv/sites/soj2/soj2/media'
-
-LOGIN_REDIRECT_URL = '/'
-
+# Misc settings
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
-
-ADMINS = (
-     ('Amarus Support', 'support@amarus.co.uk'),
-)
-
-MANAGERS = ADMINS
-
 TIME_ZONE = 'UTC'
 
-LATEST_NEWS_SLUG = 'blah'
-MIDDLEWARE_CLASSES += ("django.middleware.csrf.CsrfViewMiddleware",)
-TEMPLATE_CONTEXT_PROCESSORS += ("django.core.context_processors.static",)
+# Misc Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '%s/soj2.sqlite' % PROJECT_ROOT,
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
